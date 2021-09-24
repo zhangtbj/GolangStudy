d7b9c001d6d041c50380ebb2eeadd7562b513141
zhangtbj <zhangtbj@cn.ibm.com>
2021-09-24 14:08:39 +0800

    Update usage report example

--- ibmcloud/usagereportsv4/usagereportsv4.go
+++ ibmcloud/usagereportsv4/usagereportsv4.go
@@ -20,7 +20,12 @@ func getResourceUsageData(creds core.BearerTokenAuthenticator) (bool, error) {
 	timeUnix := time.Now().AddDate(0, 0, 1).Unix()
 	billingMonth := time.Unix(timeUnix, 0).Format("2006-01")
 
-	resourceUsageOption := usageReport.NewGetResourceUsageAccountOptions("27212a64558a41a9b19577d4232fc3f2", billingMonth)
+	// FID
+	accountID := "27212a64558a41a9b19577d4232fc3f2"
+	// GBS
+	//accountID := "17a1a02adc9f4e1393a1cb9d1a5aec8a"
+
+	resourceUsageOption := usageReport.NewGetResourceUsageAccountOptions(accountID, billingMonth)
 	resourceUsageOption.SetNames(true)
 	resourceUsageOption.SetLimit(100)
 
@@ -36,24 +41,29 @@ func getResourceUsageData(creds core.BearerTokenAuthenticator) (bool, error) {
 		}
 		resourceUsageOption.SetStart(*instancesUsage.Next.Offset)
 	}
-	fmt.Printf("GetResourceUsageAccount result count is %d", len(instanceUsages))
+	fmt.Printf("GetResourceUsageAccount result count is %d\n\n", len(instanceUsages))
 
-	accountUsageOptions := usageReport.NewGetAccountUsageOptions("27212a64558a41a9b19577d4232fc3f2", billingMonth)
+	accountUsageOptions := usageReport.NewGetAccountUsageOptions(accountID, billingMonth)
 	accountUsageOptions.SetNames(true)
-	accountUsage, response, err := usageReport.GetAccountUsage(accountUsageOptions)
+	accountUsage, _, err := usageReport.GetAccountUsage(accountUsageOptions)
 	if err != nil {
 		return false, err
 	}
-	fmt.Printf("GetAccountSummary response is %s", response)
-	fmt.Printf("GetAccountSummary result count is %d", accountUsage.Resources)
 
-	accountSummaryOptions := usageReport.NewGetAccountSummaryOptions("27212a64558a41a9b19577d4232fc3f2", billingMonth)
-	accountSummary, response, err := usageReport.GetAccountSummary(accountSummaryOptions)
+	var sumAccountUsage float64
+	for i:=0;i<len(accountUsage.Resources);i++{
+		sumAccountUsage += *accountUsage.Resources[i].BillableCost
+	}
+	//fmt.Printf("GetAccountSummary response is %s", response)
+	fmt.Printf("GetAccountUsage result count is %d\n", len(accountUsage.Resources))
+	fmt.Printf("GetAccountUsage sum cost is %f\n\n", sumAccountUsage)
+
+	accountSummaryOptions := usageReport.NewGetAccountSummaryOptions(accountID, billingMonth)
+	accountSummary, _, err := usageReport.GetAccountSummary(accountSummaryOptions)
 	if err != nil {
 		return false, err
 	}
-	fmt.Printf("GetAccountSummary response is %s", response)
-	fmt.Printf("GetAccountSummary result count is %d", accountSummary.Resources)
+	fmt.Printf("GetAccountSummary sum cost is %f\n\n", *accountSummary.Resources.BillableCost)
 
 	return true, nil
 }
